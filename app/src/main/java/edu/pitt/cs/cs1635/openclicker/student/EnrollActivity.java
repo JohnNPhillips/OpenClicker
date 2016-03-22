84d5e4f80d4ef8be48cc8a7fea2c8a12732f1fa5
John Phillips <j.phillips@email.com>
2016-03-22 11:52:38 -0400

    Allow user to enroll in classes

--- app/src/main/java/edu/pitt/cs/cs1635/openclicker/student/EnrollActivity.java
+++ app/src/main/java/edu/pitt/cs/cs1635/openclicker/student/EnrollActivity.java
@@ -1,11 +1,13 @@
 package edu.pitt.cs.cs1635.openclicker.student;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import edu.pitt.cs.cs1635.openclicker.Globals;
 import edu.pitt.cs.cs1635.openclicker.R;
 
 public class EnrollActivity extends AppCompatActivity {
@@ -15,10 +17,17 @@ public class EnrollActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enroll);
 
-        Button enrollClass = (Button)findViewById(R.id.student_enrollClass);
+        final EditText classCode = (EditText) findViewById(R.id.student_classCode);
+
+        Button enrollClass = (Button) findViewById(R.id.student_enrollClass);
         enrollClass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String code = classCode.getText().toString().trim();
+                if (!code.isEmpty()) {
+                    Globals.studentClassList.add(code);
+                }
+
                 Intent intent = new Intent(EnrollActivity.this, StudentClassListActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
