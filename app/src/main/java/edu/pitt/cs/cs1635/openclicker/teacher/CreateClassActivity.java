9ec0ee4207e2eb29ff08b6c619740ec5c7211775
John Phillips <j.phillips@email.com>
2016-03-22 13:49:07 -0400

    Add teacher create class activity

--- app/src/main/java/edu/pitt/cs/cs1635/openclicker/teacher/CreateClassActivity.java
+++ app/src/main/java/edu/pitt/cs/cs1635/openclicker/teacher/CreateClassActivity.java
@@ -5,29 +5,33 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ListView;
+import android.widget.EditText;
 
 import edu.pitt.cs.cs1635.openclicker.Globals;
 import edu.pitt.cs.cs1635.openclicker.R;
 
-public class TeacherClassListActivity extends AppCompatActivity {
+public class CreateClassActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_teacher_class_list);
+        setContentView(R.layout.activity_create_class);
+
+        final EditText className = (EditText) findViewById(R.id.teacher_className);
 
         Button createClass = (Button) findViewById(R.id.teacher_createClass);
         createClass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(TeacherClassListActivity.this, CreateClassActivity.class);
+                String code = className.getText().toString().trim();
+                if (!code.isEmpty()) {
+                    Globals.teacherClassList.add(code);
+                }
+
+                Intent intent = new Intent(CreateClassActivity.this, TeacherClassListActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
             }
         });
-
-        TeacherClassesAdapter adapter = new TeacherClassesAdapter(Globals.teacherClassList, this);
-        ListView class_list = (ListView) findViewById(R.id.teacher_class_list);
-        class_list.setAdapter(adapter);
     }
 }
