760084da41a8773649b33a40b2454060869289ed
John Phillips <j.phillips@email.com>
2016-03-23 22:43:00 -0400

    Add create question activity

--- app/src/main/java/edu/pitt/cs/cs1635/openclicker/teacher/ClassInfoActivity.java
+++ app/src/main/java/edu/pitt/cs/cs1635/openclicker/teacher/ClassInfoActivity.java
@@ -1,7 +1,10 @@
 package edu.pitt.cs.cs1635.openclicker.teacher;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import edu.pitt.cs.cs1635.openclicker.R;
 
@@ -11,5 +14,14 @@ public class ClassInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class_info);
+
+        Button createQuestion = (Button) findViewById(R.id.teacher_create_question);
+        createQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ClassInfoActivity.this, CreateQuestionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/edu/pitt/cs/cs1635/openclicker/teacher/CreateQuestionActivity.java
@@ -0,0 +1,15 @@
+package edu.pitt.cs.cs1635.openclicker.teacher;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import edu.pitt.cs.cs1635.openclicker.R;
+
+public class CreateQuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_question);
+    }
+}
