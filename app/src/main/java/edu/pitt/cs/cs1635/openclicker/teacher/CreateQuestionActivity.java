95d1f2757ae8ee38b561e2f40b43bf57277b512d
John Phillips <j.phillips@email.com>
2016-03-25 14:59:19 -0400

    Add teacher ask question activity

--- app/src/main/java/edu/pitt/cs/cs1635/openclicker/teacher/CreateQuestionActivity.java
+++ app/src/main/java/edu/pitt/cs/cs1635/openclicker/teacher/CreateQuestionActivity.java
@@ -1,7 +1,10 @@
 package edu.pitt.cs.cs1635.openclicker.teacher;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import edu.pitt.cs.cs1635.openclicker.R;
 
@@ -11,5 +14,14 @@ public class CreateQuestionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_question);
+
+        Button askQuestion = (Button) findViewById(R.id.teacher_start_question);
+        askQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CreateQuestionActivity.this, AskQuestion.class);
+                startActivity(intent);
+            }
+        });
     }
 }
