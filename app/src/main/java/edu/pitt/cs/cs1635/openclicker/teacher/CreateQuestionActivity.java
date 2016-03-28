544e98d5219410661fb424b6ee19531e17608cbe
John Phillips <j.phillips@email.com>
2016-03-28 18:22:48 -0400

    Allow only one correct answer to be selected when creating question

--- app/src/main/java/edu/pitt/cs/cs1635/openclicker/teacher/CreateQuestionActivity.java
+++ app/src/main/java/edu/pitt/cs/cs1635/openclicker/teacher/CreateQuestionActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 
 import edu.pitt.cs.cs1635.openclicker.R;
 
@@ -23,5 +24,27 @@ public class CreateQuestionActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        final RadioButton[] radios = new RadioButton[5];
+        radios[0] = (RadioButton)findViewById(R.id.question_a_correct);
+        radios[1] = (RadioButton)findViewById(R.id.question_b_correct);
+        radios[2] = (RadioButton)findViewById(R.id.question_c_correct);
+        radios[3] = (RadioButton)findViewById(R.id.question_d_correct);
+        radios[4] = (RadioButton)findViewById(R.id.question_e_correct);
+
+        View.OnClickListener radioClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Only select correct radio button
+                for (RadioButton radio : radios) {
+                    radio.setChecked(false);
+                }
+                ((RadioButton)v).setChecked(true);
+            }
+        };
+
+        for (int i = 0; i < radios.length; i++) {
+            radios[i].setOnClickListener(radioClick);
+        }
     }
 }
